cc440b161158d5b34bf9f84c94607215de662185
ZahiraParedes <zahiraparedesr@gmail.com>
2023-03-22 22:24:13 -0500

    switch

--- Grupo2.cs
+++ Grupo2.cs
@@ -26,4 +26,51 @@ public class Grupo2 : MonoBehaviour
             Debug.Log("n1 es mayor que n2");
         }
     }
+
+    void numero2()
+    {
+        int mes;
+        mes = random.range(1, 12);
+        switch (mes)
+        {
+            case 1:
+                Debug.Log("Enero");
+                break;
+            case 2:
+                Debug.Log("Febrero");
+                break;
+            case 3:
+                Debug.Log("Marzo");
+                break;
+            case 4:
+                Debug.Log("Abril");
+                break;
+            case 5:
+                Debug.Log("Mayo");
+                break;
+            case 6:
+                Debug.Log("Junio");
+                break;
+            case 7:
+                Debug.Log("Julio");
+                break;
+            case 8:
+                Debug.Log("Agosto");
+                break;
+            case 9:
+                Debug.Log("Septiembre");
+                break;
+            case 10:
+                Debug.Log("Octubre");
+                break;
+            case 11:
+                Debug.Log("Noviembre");
+                break;
+            case 12:
+                Debug.Log("Diciembre");
+                break;
+        }
+    }
+
+
 }
