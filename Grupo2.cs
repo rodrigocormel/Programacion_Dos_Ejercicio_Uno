3de26c5f188ad5bacee02382ba502ec85864e355
ZahiraParedes <zahiraparedesr@gmail.com>
2023-03-22 22:21:38 -0500

    if

--- Grupo2.cs
+++ Grupo2.cs
@@ -4,5 +4,26 @@ using UnityEngine;
 
 public class Grupo2 : MonoBehaviour
 {
-   
+    void Start()
+    {
+        numero1();
+        numero2();
+        numero3();
+
+    }
+
+    void numero1()
+    {
+        public int n1;
+        public int n2;
+
+        if (n1 < n2)
+        {
+            Debug.Log("n2 es mayor que n1");
+        }
+        else
+        {
+            Debug.Log("n1 es mayor que n2");
+        }
+    }
 }
